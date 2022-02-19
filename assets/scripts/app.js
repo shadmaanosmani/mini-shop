aa941eb40acafe2d9b165859412ffc99fd243ce7
Shadman Osmani <shadmaanosmani@outlook.com>
2022-02-19 18:46:41 +0530

    Adding ProductList, ProductItem classes.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,8 +7,31 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const productHtmlElement = document.createElement("li");
+    productHtmlElement.className = "product-item";
+    productHtmlElement.innerHTML = `
+    <div>
+      <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+      <div class="product-item__content">
+        <h2>${this.product.title}</h2>
+        <h3>\$${this.product.price}</h3>
+        <p>${this.product.description}</p>
+        <button>Add to Cart</button>
+      </div>
+    </div>
+  `;
+    return productHtmlElement;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       "A Pillow",
       "https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg",
@@ -21,29 +44,20 @@ const productList = {
       89.99,
       "A carpet which you might like - or not."
     ),
-  ],
+  ];
   render() {
     const renderHook = document.getElementById("app");
     const productHtmlList = document.createElement("ul");
     productHtmlList.className = "product-list";
     for (const product of this.products) {
-      const productHtmlElement = document.createElement("li");
-      productHtmlElement.className = "product-item";
-      productHtmlElement.innerHTML = `
-        <div>
-          <img src="${product.imageUrl}" alt="${product.title}" >
-          <div class="product-item__content">
-            <h2>${product.title}</h2>
-            <h3>\$${product.price}</h3>
-            <p>${product.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      `;
-      productHtmlList.append(productHtmlElement);
+      const productItem = new ProductItem(product);
+      const productItemHtmlElement = productItem.render();
+      productHtmlList.append(productItemHtmlElement);
     }
     renderHook.append(productHtmlList);
-  },
-};
+  }
+}
+
+const productList = new ProductList();
 
 productList.render();
